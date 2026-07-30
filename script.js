0f8e4046ab114172804fb59fbf3d563d297d31a6
rew store <rewbusiness11@gmail.com>
2026-07-30 23:01:09 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
+const judulAsli = document.title;
 
+document.addEventListener("visibilitychange", function () {
+    if (document.hidden) {
+        document.title = "👋 Jangan lupa kembali ke Sye Store!";
+    } else {
+        document.title = judulAsli;
+    }
+});
